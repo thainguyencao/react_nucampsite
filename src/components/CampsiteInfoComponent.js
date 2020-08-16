94a77d4879173fad092d0e670bb5ebb9fe03fcce
Thai Nguyen <nguyencaothai@gmail.com>
2020-08-16 01:19:34 -0700

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,38 +24,38 @@ function RenderComments({comments}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
-              <h4>Comments</h4>
-              {comments.map((comment) => (
-                <div key={comment.id}>
-                  <p>{comment.text}</p>
-                  <p>
-                    {comment.author} -{" "}
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "2-digit",
-                    }).format(new Date(Date.parse(comment.date)))}
-                  </p>
-                </div>
-              ))}
+        <h4>Comments</h4>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            <p>{comment.text}</p>
+            <p>
+              {comment.author} -{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
+            </p>
+          </div>
+        ))}
       </div>
           );
-        }
-        return <div />;
+  }
+  return <div />;
 }
 
 function CampsiteInfo(props) {
-        // check if an object with the name "campsite" (passed in via props) can be evaluated as truthy
-        if (props.campsite) { // campsite is truthy
-          return (
-            <div className="container">
-              <div className="row">
-                <RenderCampsite campsite={props.campsite} />
-                <RenderComments comments={props.campsite.comments} />
-              </div>
-            </div>
-          );
-        }
-        return <div />; // campsite not truthy --> display nothing
+  // check if an object with the name "campsite" (passed in via props) can be evaluated as truthy
+  if (props.campsite) { // campsite is truthy
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />
+        </div>
+      </div>
+    );
+  }
+  return <div />; // campsite not truthy --> display nothing
 }
 export default CampsiteInfo;

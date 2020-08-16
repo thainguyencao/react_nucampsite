774c2490a1568b3e19a0dfe042e0cf4e19172049
Thai Nguyen <nguyencaothai@gmail.com>
2020-08-16 01:46:25 -0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+            campsites: CAMPSITES
         };
     }
 
@@ -19,14 +19,22 @@ class Main extends Component {
     }
 
     render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            ); 
+        }
         return (
             <div>
-                <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-                <Footer />
-            </div>
-        );
+            <Header />
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                <Redirect to='/home' />
+            </Switch>
+            <Footer />
+        </div>
+    );
     };
 }
 

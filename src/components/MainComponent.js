18d7377bbf8de5e7513475de4ea101e15680ad89
Thai Nguyen <nguyencaothai@gmail.com>
2020-08-16 02:01:32 -0700

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,15 +5,22 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
-
     onCampsiteSelect(campsiteId) {
         this.setState({selectedCampsite: campsiteId});
     }
@@ -21,15 +28,20 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
-            ); 
-        }
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        };
         return (
             <div>
             <Header />
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                <Route exact path='/contactus' component={Contact} />
                 <Redirect to='/home' />
             </Switch>
             <Footer />

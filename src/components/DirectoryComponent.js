0953713e720410849f8c9a7a9160eb19cbc6787e
Thai Nguyen <nguyencaothai@gmail.com>
2020-09-02 17:11:47 -0700

    Fetch from Server

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDirectoryItem({campsite, onClick}) {
     return (
         <Link to={`/directory/${campsite.id}`}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
+                 <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
+           <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
             </CardImgOverlay>
         </Link>
